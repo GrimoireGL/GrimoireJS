ffa674aedd85ffc45d5b6d41c32fa4c0d8b86c03
LimeStreem <LimeStreem@gmail.com>
2015-06-11 21:49:37 -0700

    :arrow_up: implement RBO Wrapper

--- jThree/src/Core/Resources/RBO/RBO.ts
+++ jThree/src/Core/Resources/RBO/RBO.ts
@@ -1,8 +1,18 @@
 import ContextSafeResourceContainer = require('./../ContextSafeResourceContainer');
 import RBOWrapper = require('./RBOWrapper');
 import RBOInternalFormatType = require('../../../Wrapper/RBO/RBOInternalFormat');
-class RBO
+import ContextManagerBase = require('../../../Core/ContextManagerBase');
+import JThreeContext = require('../../../Core/JThreeContext')
+class RBO extends ContextSafeResourceContainer<RBOWrapper>
 {
+	constructor(context:JThreeContext,width:number,height:number,format:RBOInternalFormatType=RBOInternalFormatType.DepthComponent16)
+	{
+		super(context);
+		this.width=width;
+		this.height=height;
+		this.format=format;
+	}
+	
 	private width:number;
 	private height:number;
 	private format:RBOInternalFormatType;
@@ -20,4 +30,14 @@ class RBO
 	{
 		return this.format;
 	}
+	
+	protected getInstanceForRenderer(renderer:ContextManagerBase): RBOWrapper {
+		return new RBOWrapper(renderer,this);
+    }
+
+    protected disposeResource(resource: RBOWrapper): void {
+
+    }
 }
+
+export = RBO;
--- jThree/src/Core/Resources/RBO/RBOWrapper.ts
+++ jThree/src/Core/Resources/RBO/RBOWrapper.ts
@@ -1,10 +1,39 @@
 import ResourceWrapper = require('../ResourceWrapper');
 import ContextManagerBase = require('../../ContextManagerBase');
+import RBO = require('./RBO');
+/**
+ * Provides wrapper class for Render Buffer Object depending on particular WebGLRenderingContext.
+ * Most of user may have no reason to modify by themselves.
+ */
 class RBOWrapper extends ResourceWrapper
 {
-	constructor(contextManager:ContextManagerBase)
+	/**
+	 *	Reference to the WebGLRenderbuffer this class managing.
+	  */
+	private targetRBO:WebGLRenderbuffer;
+	
+	/**
+	 * Whethrer this class completed initialize renderbuffer already or not.
+	 */
+	private initialized:boolean=false;
+	
+	/**
+	 * The parent RBOWrapper container class.
+	 */
+	private parent:RBO;
+	
+	constructor(contextManager:ContextManagerBase,parentRBO:RBO)
 	{
 		super(contextManager);
+		this.parent=parentRBO;
+	}
+	
+	init()
+	{
+		if(this.initialized)return;
+		this.targetRBO=this.WebGLContext.CreateRenderBuffer();
+		this.WebGLContext.BindRenderBuffer(this.targetRBO);
+		this.WebGLContext.RenderBufferStorage(this.parent.Format,this.parent.Width,this.parent.Height);
 	}
 }
 export = RBOWrapper;
