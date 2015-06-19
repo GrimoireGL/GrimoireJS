d2c7ee639fa485c0c7b068b361c4b12373f3605e
pnlybubbles <pnlybubbles@gmail.com>
2015-06-19 11:58:08 +0900

    fix: "doc" gulp task will no longer use global binary

--- gulpfile.js
+++ gulpfile.js
@@ -217,7 +217,7 @@ gulp.task('gen-doc-travis',function(){
  */
 
 gulp.task('doc', function(cb) {
-  child_process.exec("typedoc --out ./ci/docs" + branch + " --module commonjs --target es5 --name jThree ./jThree/src/", cb);
+  child_process.exec("./node_modules/.bin/typedoc --out ./ci/docs" + branch + " --module commonjs --target es5 --name jThree ./jThree/src/", cb);
   return void 0;
 });
 
