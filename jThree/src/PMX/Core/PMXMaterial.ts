d846d1169aab518e7bbe65a30ddec64db6be1396
LimeStreem <LimeStreem@gmail.com>
2015-08-15 13:56:02 -0700

    :sparkles: fix PMXMaterial

--- jThree/src/PMX/Core/PMXMaterial.ts
+++ jThree/src/PMX/Core/PMXMaterial.ts
@@ -25,7 +25,8 @@ declare function require(string): string;
 /**
  * the materials for PMX.
  */
-class PMXMaterial extends Material {
+class PMXMaterial extends Material
+{
     protected program: Program;
 
     protected edgeProgram: Program;
@@ -37,14 +38,16 @@ class PMXMaterial extends Material {
     /**
      * Count of verticies
      */
-    public get VerticiesCount() {
+    public get VerticiesCount()
+    {
         return this.verticiesCount;
     }
 
     /**
      * Offset of verticies in index buffer
      */
-    public get VerticiesOffset() {
+    public get VerticiesOffset()
+    {
         return this.verticiesOffset;
     }
 
@@ -53,7 +56,8 @@ class PMXMaterial extends Material {
         return this.parentModel;
     }
 
-    public get Diffuse(): Color4 {
+    public get Diffuse(): Color4
+    {
         return this.diffuse;
     }
 
@@ -85,16 +89,18 @@ class PMXMaterial extends Material {
 
     public mulMorphParam: PmxMaterialMorphParamContainer;
 
-    public get PassCount(): number {
-        return 1; //this.edgeColor == null ? 1 : 2;
-    }
-
-    public get SelfShadow():boolean
+    public get PassCount(): number
     {
-        return (this.pmxData.Materials[this.materialIndex].drawFlag & 0x04)>0;
+        return this.edgeColor == null ? 1 : 2;
     }
 
-    constructor(pmx: PMXModel, index: number, offset: number, directory: string) {
+    public get SelfShadow(): boolean
+    {
+        return (this.pmxData.Materials[this.materialIndex].drawFlag & 0x04) > 0;
+    }
+
+    constructor(pmx: PMXModel, index: number, offset: number, directory: string)
+    {
         super();
         this.addMorphParam = new PmxMaterialMorphParamContainer(1);
         this.mulMorphParam = new PmxMaterialMorphParamContainer(0);
@@ -119,62 +125,63 @@ class PMXMaterial extends Material {
         this.edgeProgram = this.loadProgram("jthree.shaders.vertex.pmx.edge", "jthree.shaders.fragment.pmx.edge", "jthree.programs.pmx.edge", vs, fs);
         this.sphere = this.loadPMXTexture(materialData.sphereTextureIndex, "sphere", directory);
         this.texture = this.loadPMXTexture(materialData.textureIndex, "texture", directory);
-        if (materialData.sharedToonFlag == 0) {// not shared texture
+        if (materialData.sharedToonFlag == 0)
+        {// not shared texture
             this.toon = this.loadPMXTexture(materialData.targetToonIndex, "toon", directory);
         }
         this.setLoaded();
     }
 
-    public configureMaterial(scene: Scene, renderer: RendererBase, object: SceneObject, texs: ResolvedChainInfo, pass?: number): void {
-/*        if (pass == 1) {
+    public configureMaterial(scene: Scene, renderer: RendererBase, object: SceneObject, texs: ResolvedChainInfo, pass?: number): void
+    {
+        if (pass == 1)
+        {
             this.configureEdgeMaterial(renderer, object);
             return;
-        }*/
+        }
         if (!this.program) return;
         super.configureMaterial(scene, renderer, object, texs);
         renderer.GLContext.Enable(GLFeatureType.DepthTest);
         renderer.GLContext.Enable(GLFeatureType.Blend);
         renderer.GLContext.BlendFunc(BlendFuncParamType.SrcAlpha, BlendFuncParamType.OneMinusSrcAlpha);
-        var id = renderer.ID;
         var geometry = <PMXGeometry>object.Geometry;
         var programWrapper = this.program.getForContext(renderer.ContextManager);
         var v = object.Transformer.calculateMVPMatrix(renderer);
-
         programWrapper.register(
-        {
-            attributes: {
-                position: geometry.PositionBuffer,
-                normal: geometry.NormalBuffer,
-                uv: geometry.UVBuffer,
-                boneWeights: geometry.boneWeightBuffer,
-                boneIndicies: geometry.boneIndexBuffer
+            {
+                attributes: {
+                    position: geometry.PositionBuffer,
+                    normal: geometry.NormalBuffer,
+                    uv: geometry.UVBuffer,
+                    boneWeights: geometry.boneWeightBuffer,
+                    boneIndicies: geometry.boneIndexBuffer
                 },
-            uniforms: {
-                u_light: {
-                    type: "texture",
-                    value: texs["LIGHT"],
-                    register: 0
-                },
-                u_texture: {
-                    type: "texture",
-                    value: this.texture,
-                    register: 1
-                },
-                u_toon: {
-                    type: "texture",
-                    value: this.toon,
-                    register: 2
-                },
-                u_sphere: {
-                    type: "texture",
-                    value: this.sphere,
-                    register: 3
-                },
+                uniforms: {
+                    u_light: {
+                        type: "texture",
+                        value: texs["LIGHT"],
+                        register: 0
+                    },
+                    u_texture: {
+                        type: "texture",
+                        value: this.texture,
+                        register: 1
+                    },
+                    u_toon: {
+                        type: "texture",
+                        value: this.toon,
+                        register: 2
+                    },
+                    u_sphere: {
+                        type: "texture",
+                        value: this.sphere,
+                        register: 3
+                    },
                     u_boneMatricies: {
                         type: "texture",
                         value: this.parentModel.Skeleton.MatrixTexture,
                         register: 4
-                },
+                    },
                     u_textureUsed: {
                         type: "integer",
                         value: this.texture == null || this.texture.ImageSource == null ? 0 : 1
@@ -208,40 +215,61 @@ class PMXMaterial extends Material {
                         value: this.parentModel.Skeleton.BoneCount
                     }
                 }
+            });
+        geometry.bindIndexBuffer(renderer.ContextManager);
+    }
+
+    private configureEdgeMaterial(renderer: RendererBase, object: SceneObject): void
+    {
+        if (!this.program) return;
+        renderer.GLContext.Enable(GLFeatureType.CullFace);
+        renderer.GLContext.CullFace(GLCullMode.Front);
+        var geometry = <PMXGeometry> object.Geometry;
+        var programWrapper = this.edgeProgram.getForContext(renderer.ContextManager);
+        programWrapper.register({
+            attributes: {
+                position: geometry.PositionBuffer,
+                edgeScaling: geometry.edgeSizeBuffer,
+                boneWeights: geometry.boneWeightBuffer,
+                boneIndicies: geometry.boneIndexBuffer
+            },
+            uniforms: {
+                u_boneMatricies: {
+                    type: "texture", register: 0, value: this.ParentModel.Skeleton.MatrixTexture
+                },
+                matVP: {
+                    type: "matrix", value: Matrix.multiply(renderer.Camera.ProjectionMatrix, renderer.Camera.ViewMatrix)
+                },
+                u_edgeSize: {
+                    type: "float", value: PmxMaterialMorphParamContainer.calcMorphedSingleValue(this.edgeSize, this.addMorphParam, this.mulMorphParam, (t) => t.edgeSize)
+                },
+                u_edgeColor: {
+                    type: "vector", value: PmxMaterialMorphParamContainer.calcMorphedVectorValue(
+                        this.edgeColor.toVector(), this.addMorphParam, this.mulMorphParam, (t) => t.edgeColor, 4)
+                }
+                ,
+                u_boneCount: {
+                    type: "float", value: this.parentModel.Skeleton.BoneCount
+                }
+            }
         });
         geometry.bindIndexBuffer(renderer.ContextManager);
     }
 
-    private configureEdgeMaterial(renderer: RendererBase, object: SceneObject): void {
-        if (!this.program) return;
-        renderer.GLContext.Enable(GLFeatureType.CullFace);
-        renderer.GLContext.CullFace(GLCullMode.Front);
-        var id = renderer.ID;
-        var geometry = <PMXGeometry> object.Geometry;
-        var programWrapper = this.edgeProgram.getForContext(renderer.ContextManager);
-        programWrapper.useProgram();
-        programWrapper.registerTexture(renderer, this.parentModel.Skeleton.MatrixTexture, 0, "u_boneMatricies");
-        programWrapper.setAttributeVerticies("position", geometry.PositionBuffer.getForRenderer(renderer.ContextManager));
-        programWrapper.setAttributeVerticies("edgeScaling", geometry.edgeSizeBuffer.getForRenderer(renderer.ContextManager));
-        programWrapper.setUniformMatrix("matVP", Matrix.multiply(renderer.Camera.ProjectionMatrix, renderer.Camera.ViewMatrix));
-        programWrapper.setUniform1f("u_edgeSize", PmxMaterialMorphParamContainer.calcMorphedSingleValue(this.edgeSize, this.addMorphParam, this.mulMorphParam, (t) => t.edgeSize));
-        programWrapper.setUniformVector("u_edgeColor", PmxMaterialMorphParamContainer.calcMorphedVectorValue(this.edgeColor.toVector(), this.addMorphParam, this.mulMorphParam, (t) => t.edgeColor, 4));
-        programWrapper.setAttributeVerticies("boneWeights", geometry.boneWeightBuffer.getForRenderer(renderer.ContextManager));
-        programWrapper.setAttributeVerticies("boneIndicies", geometry.boneIndexBuffer.getForRenderer(renderer.ContextManager));
-        programWrapper.setUniform1f("u_boneCount", this.parentModel.Skeleton.BoneCount);
-        geometry.bindIndexBuffer(renderer.ContextManager);
-    }
-
-    private loadPMXTexture(index: number, prefix: string, directory: string): Texture {
+    private loadPMXTexture(index: number, prefix: string, directory: string): Texture
+    {
         if (index < 0) return null;
         var rm = JThreeContextProxy.getJThreeContext().ResourceManager;
         var resourceName = "jthree.pmx." + prefix + "." + index;
-        if (rm.getTexture(resourceName)) {
+        if (rm.getTexture(resourceName))
+        {
             return rm.getTexture(resourceName);
-        } else {
+        } else
+        {
             var texture = rm.createTextureWithSource(resourceName, null);
             var img = new Image();
-            img.onload = () => {
+            img.onload = () =>
+            {
                 texture.ImageSource = img;
             };
             img.src = directory + this.pmxData.Textures[index];
@@ -249,16 +277,19 @@ class PMXMaterial extends Material {
         }
     }
 
-    public get Priorty(): number {
+    public get Priorty(): number
+    {
         return 100 + this.materialIndex;
     }
 
-    public getDrawGeometryLength(geo: Geometry): number {
-        return this.diffuse.A>0?this.VerticiesCount:0;
+    public getDrawGeometryLength(geo: Geometry): number
+    {
+        return this.diffuse.A > 0 ? this.VerticiesCount : 0;
     }
 
-    public getDrawGeometryOffset(geo: Geometry): number {
-        return this.VerticiesOffset*4;
+    public getDrawGeometryOffset(geo: Geometry): number
+    {
+        return this.VerticiesOffset * 4;
     }
 }
 
