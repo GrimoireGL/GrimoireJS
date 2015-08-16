1daab66b98617654e1abebd657eeed1f1f54858b
LimeStreem <LimeStreem@gmail.com>
2015-08-16 15:59:49 -0700

    :sparkles: add renderstage configurator base class

---
+++ jThree/src/Core/Renderers/RendererConfigurator/RendererConfiguratorBase.ts
@@ -0,0 +1,14 @@
+﻿import GeneraterInfo = require("../TextureGeneraters/GeneraterInfo");
+import RenderStageChain = require("../RenderStageChain");
+class RendererConfiguratorBase
+{
+    public get TextureBuffers():GeneraterInfo {
+        return null;
+    }
+
+    public get StageChain():RenderStageChain {
+        return null;
+    }
+}
+
+export = RendererConfiguratorBase;
