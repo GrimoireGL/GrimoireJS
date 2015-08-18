acd131905c03cb60fa5aa7677ffe176125b13648
LimeStreem <LimeStreem@gmail.com>
2015-08-17 19:49:51 -0700

    fix bug of light texture

--- jThree/src/Core/Shaders/Light/PointLightFragmentChunk.glsl
+++ jThree/src/Core/Shaders/Light/PointLightFragmentChunk.glsl
@@ -1,7 +1,7 @@
 ﻿vec3 calcPointLight(vec3 position,vec3 normal,int i)
 {
   vec3 accum=vec3(0,0,0);
-  vec3 color = getLightParameter(i,0).xyz;
+  vec3 color = getLightParameter(i,0).yzw;
   vec3 lpos = getLightParameter(i,1).xyz;
   vec2 param = getLightParameter(i,2).xy;
     float l=distance(lpos,position);//calc distance between light and fragment in view space
