d27539690b0297ef012af54fb9e1472765b68e2d
LimeStreem <LimeStreem@gmail.com>
2015-08-25 11:38:27 -0700

    :bug: fix normal buffer problem

--- jThree/src/Core/Scene.ts
+++ jThree/src/Core/Scene.ts
@@ -7,14 +7,18 @@ import AssociativeArray = require('../Base/Collections/AssociativeArray');
 import LightBase = require('./Light/LightBase')
 import Delegates =require('../Base/Delegates')
 import LightRegister = require('./Light/LightRegister');
-import PointLight = require("./Light/PointLight"); 
+import PointLight = require("./Light/PointLight");
+import DirectionalLight = require("./Light/DirectionalLight");
 class Scene extends jThreeObjectWithID {
     constructor() {
         super();
         this.enabled = true;
         this.lightRegister = new LightRegister(this);
+        //TODO Remove parameter registration
         var pointParam = PointLight.TypeDefinition;
+        var dp = DirectionalLight.TypeDefinition;
         this.lightRegister.addLightType(pointParam.requiredParamCount, pointParam.shaderfuncName, pointParam.shaderfragmentCode, pointParam.typeName);
+        this.lightRegister.addLightType(dp.requiredParamCount,dp.shaderfuncName,dp.shaderfragmentCode,dp.typeName);
         console.log(this.lightRegister.ShaderCodeComposer.ShaderCode);
     }
 
