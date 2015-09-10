00025bbef27e76f62f2e14584667d51f1a05bb36
LimeStreem <LimeStreem@gmail.com>
2015-09-10 15:51:42 -0700

    :sparkles: add point light specular fragment shader

--- jThree/src/Core/Shaders/Light/PointLightSpecularFragmentChunk.glsl
+++ jThree/src/Core/Shaders/Light/PointLightSpecularFragmentChunk.glsl
@@ -1,4 +1,11 @@
 vec3 calcPointLight(vec3 position,vec3 normal,int i,vec3 specular,float specularCoefficient)
 {
-  return vec3(0,0,0);
+  vec3 accum=vec3(0,0,0);
+  vec3 color = getLightParameter(i,0).yzw;
+  vec3 lpos = getLightParameter(i,1).xyz;
+  vec2 param = getLightParameter(i,2).xy;
+  vec3 hv = normalize(normalize(lpos-position)+normalize(-position));
+  float l = distance(position,lpos);
+  accum += pow(max(0.,dot(hv,normal)),specularCoefficient)*pow(max(0.,1.-l/param.x),param.y)*specular*color;
+  return accum;
 }
