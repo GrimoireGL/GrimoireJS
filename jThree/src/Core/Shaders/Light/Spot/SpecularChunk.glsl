26d889ec6724d9936fb308e2f7b1c3b2bb753da6
LimeStreem <LimeStreem@gmail.com>
2015-09-12 18:29:28 -0700

    :arrow_up: add spot light specular light shader

--- jThree/src/Core/Shaders/Light/Spot/SpecularChunk.glsl
+++ jThree/src/Core/Shaders/Light/Spot/SpecularChunk.glsl
@@ -5,5 +5,10 @@ vec3 calcSpotLight(vec3 position,vec3 normal,int i,vec3 specular,float specularC
   vec3 lpos = getLightParameter(i,1).xyz;
   vec3 ldir = getLightParameter(i,2).xyz;
   vec3 params = getLightParameter(i,3).xyz;
+  vec3 hv = normalize(normalize(lpos-position)+normalize(-position));
+  vec3 l2p = normalize(position - lpos);
+  accum += color * specular
+    * pow(max(0.,dot(hv,normal)),specularCoefficient) //Normal attenuation
+   * pow(max(0.,min(1.,dot(l2p,ldir)/(params.x-params.y) - params.y/(params.x - params.y))),params.z); // spot light range attenuation
   return accum;
 }
