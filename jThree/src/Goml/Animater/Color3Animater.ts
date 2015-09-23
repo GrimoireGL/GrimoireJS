162dc79275693d5d8cc7c0a25967e6cf4eb11a37
LimeStreem <LimeStreem@gmail.com>
2015-09-22 23:07:05 -0700

    :sparkles: add color3 animation

---
+++ jThree/src/Goml/Animater/Color3Animater.ts
@@ -0,0 +1,15 @@
+import AnimagterBase = require("./AnimaterBase");
+import Color3 = require("../../Base/Color/Color3");
+class Color3Animater extends AnimagterBase
+{
+
+  protected updateAnimation(progress:number):void
+  {
+    var b=<Color3>this.beginValue;
+    var e=<Color3>this.endValue;
+    var ef=this.easingFunction.Ease;
+    this.targetAttribute.Value=new Color3(ef(b.R,e.R,progress),ef(b.G,e.G,progress),ef(b.B,e.B,progress));
+  }
+}
+
+export=Color3Animater;
