59b752b186650720025934117faa6c50b32d28ce
LimeStreem <LimeStreem@gmail.com>
2015-11-18 01:28:56 -0800

    :sparkles: add scene light tag

--- jThree/src/Core/Light/DefaultLightTypeList.ts
+++ jThree/src/Core/Light/DefaultLightTypeList.ts
@@ -3,5 +3,6 @@ export =
   require("./Impl/AreaLight"),
   require("./Impl/DirectionalLight.ts"),
   require("./Impl/PointLight.ts"),
-  require("./Impl/SpotLight.ts")
+  require("./Impl/SpotLight.ts"),
+  require("./Impl/SceneLight.ts")
 ]
---
+++ jThree/src/Core/Light/Impl/SceneLight.ts
@@ -0,0 +1,47 @@
+import Vector3 = require('../../../Math/Vector3');
+import LightBase = require('./../LightBase');
+import Scene = require('../../Scene');
+import Matrix = require('../../../Math/Matrix');
+import LightTypeDeclaration = require("./../LightTypeDeclaration");
+import RendererBase = require("../../Renderers/RendererBase");
+import glm = require("gl-matrix");
+
+/**
+ * Provides area light feature.
+ * Parameters:
+ * X:TYPE ID ,XYZ:COLOR
+ */
+class SceneLight extends LightBase {
+	constructor(scene: Scene) {
+		super(scene);
+    }
+
+    public getParameters(renderer:RendererBase): number[] {
+        return [this.Color.R * this.intensity, this.Color.G * this.intensity, this.Color.B * this.intensity];
+		}
+
+		private calcAxis(x:number,y:number,z:number,bp:Vector3)
+		{
+			return new Vector3(x + bp.X , y + bp.Y, -z + bp.Z);
+		}
+
+	public intensity: number = 1.0;
+
+
+	public get LightType(): string {
+		return "jthree.lights.scenelight";
+    }
+
+    public static get TypeDefinition(): LightTypeDeclaration
+    {
+        return {
+            typeName: "jthree.lights.scenelight",
+            requiredParamCount: 1,
+            shaderfuncName: "calcSceneLight",
+            diffuseFragmentCode: require('../../Shaders/Light/Scene/DiffuseChunk.glsl'),
+            specularFragmentCode: require('../../Shaders/Light/Scene/SpecularChunk.glsl')
+        };
+    }
+}
+
+export = SceneLight;
