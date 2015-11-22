de475fdcbb7cc7d267923409d502135a5bd92617
LimeStreem <LimeStreem@gmail.com>
2015-11-21 20:10:35 -0800

    :bug: recover shadow map rendering

--- jThree/src/Core/Light/Impl/DirectionalLight.ts
+++ jThree/src/Core/Light/Impl/DirectionalLight.ts
@@ -63,7 +63,8 @@ class DirectionalLight extends ShadowDroppableLight {
 			this.generateLightviewMatrix(renderer.Camera);
 			this.USM(renderer);
 		  //glm.mat4.multiply(this.lightMatrixCache.rawElements,this.lightMatrixCache.rawElements,Matrix.scale(new Vector3(1,1,-1)).rawElements);
-			this.updateLightProjection(renderer,Matrix.multiply(Matrix.perspective(1.0,1,0.1,30),Matrix.lookAt(this.Transformer.Position,Vector3.add(this.Transformer.Position,this.Transformer.forward),Vector3.YUnit)));
+		  //Matrix.lookAt(this.Transformer.Position,Vector3.add(this.Transformer.Position,this.Transformer.forward),Vector3.YUnit))
+			this.updateLightProjection(renderer,Matrix.multiply(Matrix.perspective(1.0,1,0.1,30),Matrix.lookAt(new Vector3(0,0,5),new Vector3(0,0,0),new Vector3(0,1,0))));
 			// var m = Matrix.multiply(this.shadowProjectionMatrixCache,this.shadowViewMatrixCache);
 			// this.updateLightProjection(renderer,m);
 		}
