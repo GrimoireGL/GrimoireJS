dcbbe050259e6e50cd6e549a70a049e5dc50dc1e
pnlybubbles <pnlybubbles@gmail.com>
2016-01-09 16:37:05 +0900

    fix: cubemap is no longer disappeared

--- jThree/src/Goml/GomlAttribute.ts
+++ jThree/src/Goml/GomlAttribute.ts
@@ -62,7 +62,7 @@ class GomlAttribute extends JThreeObjectEEWithID {
 
   public set Value(val: any) {
     console.log('setattr', this.Name, val);
-    if (this.constant && this.value === undefined) {
+    if (this.constant && this.value !== undefined) {
       console.warn(`attribute "${this.ID}" is immutable`)
       return;
     }
