3a809481f1b4436390a5dc6dfb893af944b4edaf
pnlybubbles <pnlybubbles@gmail.com>
2016-01-14 13:12:07 +0900

    chore: remove tsify and use gulp-typescript to compile

--- jThree/src/PMX/Goml/PMXBonesNode.ts
+++ jThree/src/PMX/Goml/PMXBonesNode.ts
@@ -2,7 +2,7 @@ import GomlTreeNodeBase = require("../../Goml/GomlTreeNodeBase");
 import PMXNode = require("./PMXNode");
 class PMXBonesNode extends GomlTreeNodeBase {
   constructor(elem: HTMLElement, parent: GomlTreeNodeBase) {
-    super(elem, parent);
+    super();
     this.targetPMXNode = <PMXNode>parent;
   }
 
