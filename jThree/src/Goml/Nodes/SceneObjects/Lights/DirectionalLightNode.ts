5bf0e9104f51ce1e31587f56e0a069d92ee32645
LimeStreem <LimeStreem@gmail.com>
2016-01-17 11:21:37 +0900

    :sparkles: fixed directional light

--- jThree/src/Goml/Nodes/SceneObjects/Lights/DirectionalLightNode.ts
+++ jThree/src/Goml/Nodes/SceneObjects/Lights/DirectionalLightNode.ts
@@ -1,42 +1,33 @@
-import GomlTreeNodeBase = require("../../../GomlTreeNodeBase");
-import SceneObjectNodeBase = require("../SceneObjectNodeBase");
-import GomlTreeSceneNode = require("../../SceneNode");
-import LightNodeBase = require('./LightNodeBase');
+import DirectionalLight = require("../../../../Core/Light/Impl/DirectionalLight");
+import LightNodeBase = require("./LightNodeBase");
 // import DirectionalLight = require('../../../../Core/Light/Impl/DirectionalLight');
-import LightBase = require('../../../../Core/Light/LightBase');
+import LightBase = require("../../../../Core/Light/LightBase");
 
 class DirectionalLightNode extends LightNodeBase {
-  private targetLight: LightBase;
+  private targetLight: DirectionalLight;
 
   constructor() {
     super();
-    // this.attributes.defineAttribute({
-    //   "intensity": {
-    //     value: 1,
-    //     converter: "float",
-    //     onchanged: (attr) => {
-    //       this.targetLight.intensity = attr.Value;
-    //     }
-    //   },
-    //   "shadow": {
-    //     value: false,
-    //     converter: "boolean",
-    //     onchanged: (attr) => {
-    //       this.targetLight.isShadowDroppable = attr.Value;
-    //     }
-    //   },
-    //   "bias": {
-    //     value: 0.01,
-    //     converter: "float",
-    //     onchanged: (attr) => {
-    //       this.targetLight.bias = attr.Value;
-    //     }
-    //   }
-    // });
+    this.attributes.defineAttribute({
+      "intensity": {
+        value: 1,
+        converter: "float",
+        onchanged: (attr) => {
+          this.targetLight.intensity = attr.Value;
+        }
+      },
+      "bias": {
+        value: 0.01,
+        converter: "float",
+        onchanged: (attr) => {
+          this.targetLight.bias = attr.Value;
+        }
+      }
+    });
   }
 
   protected constructLight(): LightBase {
-    //this.targetLight = new DirectionalLight(this.ContainedSceneNode.targetScene);
+    this.targetLight = new DirectionalLight(this.ContainedSceneNode.targetScene);
     return this.targetLight;
   }
 }
