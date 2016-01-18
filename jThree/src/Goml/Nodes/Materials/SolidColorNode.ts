f6bd7edc3a5e2839afd427ec61326e2c2a983020
pnlybubbles <pnlybubbles@gmail.com>
2016-01-18 12:46:53 +0900

    refactor: Materials

--- jThree/src/Goml/Nodes/Materials/SolidColorNode.ts
+++ jThree/src/Goml/Nodes/Materials/SolidColorNode.ts
@@ -1,21 +1,18 @@
-import BasicMaterial = require("../../../Core/Materials/Base/BasicMaterial");
-import GomlTreeNodeBase = require("../../GomlTreeNodeBase");
-import MaterialNodeBase = require('./MaterialNodeBase');
-import Material = require('../../../Core/Materials/Material');
+import MaterialNodeBase = require("./MaterialNodeBase");
+import Material = require("../../../Core/Materials/Material");
 
 class SolidColorNode extends MaterialNodeBase {
-  public material: BasicMaterial;
-
   constructor() {
     super();
   }
 
-  protected ConstructMaterial(): Material {
-    this.material = this.__getMaterialFromMatName("jthree.basic.solid");
-    return this.material;
+  protected onMount(): void {
+    super.onMount();
   }
 
-
+  protected ConstructMaterial(): Material {
+    return this.__getMaterialFromMatName("jthree.basic.solid");
+  }
 }
 
 export = SolidColorNode;
