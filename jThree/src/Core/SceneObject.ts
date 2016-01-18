b465ea144e4392dcad7f143e666d438536252f6f
pnlybubbles <pnlybubbles@gmail.com>
2016-01-18 11:05:11 +0900

    feat: add remove child method of scene hierarchy.

--- jThree/src/Core/SceneObject.ts
+++ jThree/src/Core/SceneObject.ts
@@ -3,7 +3,7 @@ import IParentSceneChangedEventArgs = require("./IParentSceneChangedEventArgs");
 import Material = require("./Materials/Material");
 import Delegates = require("../Base/Delegates");
 import Geometry = require("./Geometries/Base/Geometry");
-import Scene=require('./Scene');
+import Scene = require('./Scene');
 import JThreeCollection = require("../Base/JThreeCollection");
 import Transformer = require("./Transform/Transformer");
 import JThreeEvent = require("../Base/JThreeEvent");
@@ -12,201 +12,215 @@ import ISceneObjectStructureChangedEventArgs = require("./ISceneObjectChangedEve
  * This is most base class for SceneObject.
  * SceneObject is same as GameObject in Unity.
  */
-class SceneObject extends JThreeObjectWithID
-{
+class SceneObject extends JThreeObjectWithID {
 
-  constructor(transformer?:Transformer)
-  {
+  public name: string;
+
+  protected geometry: Geometry;
+
+  protected transformer: Transformer;
+
+  private onStructureChangedEvent: JThreeEvent<ISceneObjectStructureChangedEventArgs> = new JThreeEvent<ISceneObjectStructureChangedEventArgs>();
+
+  private materialChanagedHandler: Delegates.Action2<Material, SceneObject>[] = [];
+
+  private materials: { [materialGroup: string]: JThreeCollection<Material> } = {};
+
+  /**
+   * Parent of this SceneObject.
+   */
+  private parent: SceneObject;
+
+  /**
+   * Contains the children.
+   */
+  private children: SceneObject[] = [];
+
+  /**
+   * Contains the parent scene containing this SceneObject.
+   */
+  private parentScene: Scene;
+
+  constructor(transformer?: Transformer) {
     super();
-    this.transformer=transformer||new Transformer(this);
+    this.transformer = transformer || new Transformer(this);
     this.name = this.ID;
   }
 
-    private onStructureChangedEvent:JThreeEvent<ISceneObjectStructureChangedEventArgs> = new JThreeEvent<ISceneObjectStructureChangedEventArgs>();
+  /**
+  * Getter for children
+  */
+  public get Children(): SceneObject[] {
+    return this.children;
+  }
 
-    public name:string;
-
-    private materialChanagedHandler:Delegates.Action2<Material,SceneObject>[]=[];
-
-    private materials:{[materialGroup:string]:JThreeCollection<Material>}= {};
-
-    /**
-     * Contains the children.
-     */
-    private children:SceneObject[] =[];
-
-    /**
-    * Getter for children
-    */
-    public get Children():SceneObject[]{
-      return this.children;
+  /**
+   * add SceneObject to children.
+   * @param {SceneObject} obj [description]
+   */
+  public addChild(obj: SceneObject): void {
+    this.children.push(obj);
+    obj.parent = this;
+    obj.Transformer.updateTransform();
+    const eventArg = {
+      owner: this,
+      scene: this.ParentScene,
+      isAdditionalChange: true,
+      changedSceneObject: obj,
+      changedSceneObjectID: obj.ID
+    };
+    this.onStructureChangedEvent.fire(this, eventArg);
+    this.onChildrenChanged();
+    obj.onParentChanged();
+    if (this.ParentScene) {
+      this.ParentScene.notifySceneObjectChanged(eventArg);
     }
+  }
 
-    public addChild(obj:SceneObject):void
-    {
-      this.children.push(obj);
-      obj.parent=this;
-      obj.Transformer.updateTransform();
-      var eventArg = {
-        owner:this,
-        scene:this.ParentScene,
-        isAdditionalChange:true,
-        changedSceneObject:obj,
-        changedSceneObjectID:obj.ID
+  /**
+   * remove SceneObject from children.
+   * @param {SceneObject} obj [description]
+   */
+  public removeChild(obj: SceneObject): void {
+    const childIndex = this.children.indexOf(obj);
+    if (childIndex !== -1) {
+      this.children.splice(childIndex, 1);
+      const eventArg = {
+        owner: this,
+        scene: this.ParentScene,
+        isAdditionalChange: false,
+        changedSceneObject: obj,
+        changedSceneObjectID: obj.ID
       };
-      this.onStructureChangedEvent.fire(this,eventArg);
-      this.onChildrenChanged();
+      this.onStructureChangedEvent.fire(this, eventArg);
       obj.onParentChanged();
-      if(this.ParentScene)this.ParentScene.notifySceneObjectChanged(eventArg);
+      if (this.ParentScene) {
+        this.ParentScene.notifySceneObjectChanged(eventArg);
+      }
     }
+  }
 
-    /**
-     * Parent of this SceneObject.
-     */
-    private parent:SceneObject;
+  /**
+   * remove this SceneObject from parent.
+   */
+  public remove(): void {
+    this.parent.removeChild(this);
+  }
 
-    public get Parent():SceneObject
-    {
-      return this.parent;
-    }
+  public get Parent(): SceneObject {
+    return this.parent;
+  }
 
-    /**
-     * Contains the parent scene containing this SceneObject.
-     */
-    private parentScene:Scene;
-
-     /**
-     * The Getter for the parent scene containing this SceneObject.
-     */
-    public get ParentScene():Scene
-    {
-        if(!this.parentScene)
-        {
-            if(!this.parent)
-            {
-                return null;
-            }else{
-                this.ParentScene=this.parent.ParentScene;//Retrieve and cache parent scene
-                return this.parentScene;
-            }
-        }else{
-            //The parent scene was already cached.
-            return this.parentScene;
-        }
-    }
-
-     /**
-     * The Getter for the parent scene containing this SceneObject.
-     */
-
-    public set ParentScene(scene:Scene)
-    {
-        if(scene == this.parentScene)return;
-        const lastScene = this.parentScene;
-        this.parentScene=scene;
-        // if(!this.parent||this.parent.ParentScene.ID!=scene.ID)
-        //     console.error("There is something wrong in Scene structure.");
-        //insert recursively to the children this SceneObject contains.
-        this.children.forEach((v)=>{
-            v.ParentScene=scene;
-        });
-        this.onParentSceneChanged({
-          lastParentScene:lastScene,
-          currentParentScene:this.parentScene
-        });
-    }
-
-    public onMaterialChanged(func:Delegates.Action2<Material,SceneObject>): void {
-        this.materialChanagedHandler.push(func);
-    }
-    /**
-     * すべてのマテリアルに対して処理を実行します。
-     */
-    public eachMaterial(func:Delegates.Action1<Material>): void {
-      for(let material in this.materials)
-        this.materials[material].each(e=>func(e));
-    }
-
-    public addMaterial(mat: Material): void
-    {
-        if(!this.materials[mat.MaterialGroup])
-        {
-            this.materials[mat.MaterialGroup] = new JThreeCollection<Material>();
-        }
-        this.materials[mat.MaterialGroup].insert(mat);
-    }
-
-    public getMaterial(matGroup:string):Material
-    {
-        if(this.materials[matGroup])
-        {
-            var a=this.materials[matGroup];
-            var ret=null;
-            a.each((e)=>{
-                ret=e;
-                return;
-            });
-            return ret;
-        }
+  /**
+  * The Getter for the parent scene containing this SceneObject.
+  */
+  public get ParentScene(): Scene {
+    if (!this.parentScene) {
+      if (!this.parent) {
         return null;
-    }
-
-    public getMaterials(matGroup:string):Material[]
-    {
-      if(this.materials[matGroup])
-      {
-        return this.materials[matGroup].asArray();
+      } else {
+        this.ParentScene = this.parent.ParentScene; // Retrieve and cache parent scene
+        return this.parentScene;
       }
-      return [];
+    } else {
+      //The parent scene was already cached.
+      return this.parentScene;
     }
+  }
 
+  /**
+  * The Getter for the parent scene containing this SceneObject.
+  */
 
-    protected geometry:Geometry;
+  public set ParentScene(scene: Scene) {
+    if (scene == this.parentScene) return;
+    const lastScene = this.parentScene;
+    this.parentScene = scene;
+    // if(!this.parent||this.parent.ParentScene.ID!=scene.ID)
+    //     console.error("There is something wrong in Scene structure.");
+    //insert recursively to the children this SceneObject contains.
+    this.children.forEach((v) => {
+      v.ParentScene = scene;
+    });
+    this.onParentSceneChanged({
+      lastParentScene: lastScene,
+      currentParentScene: this.parentScene
+    });
+  }
 
-    public get Geometry():Geometry
-    {
-      return this.geometry;
+  public onMaterialChanged(func: Delegates.Action2<Material, SceneObject>): void {
+    this.materialChanagedHandler.push(func);
+  }
+  /**
+   * すべてのマテリアルに対して処理を実行します。
+   */
+  public eachMaterial(func: Delegates.Action1<Material>): void {
+    for (let material in this.materials) {
+      this.materials[material].each((e) => func(e));
     }
+  }
 
-    public set Geometry(geo:Geometry)
-    {
-        this.geometry=geo;
+  public addMaterial(mat: Material): void {
+    if (!this.materials[mat.MaterialGroup]) {
+      this.materials[mat.MaterialGroup] = new JThreeCollection<Material>();
     }
+    this.materials[mat.MaterialGroup].insert(mat);
+  }
 
-    protected transformer:Transformer;
-
-    public get Transformer():Transformer
-    {
-      return this.transformer;
+  public getMaterial(matGroup: string): Material {
+    if (this.materials[matGroup]) {
+      const a = this.materials[matGroup];
+      let ret = null;
+      a.each((e) => {
+        ret = e;
+        return;
+      });
+      return ret;
     }
+    return null;
+  }
 
-    public callRecursive(action:Delegates.Action1<SceneObject>)
-    {
-      if(this.children)
-      {
+  public getMaterials(matGroup: string): Material[] {
+    if (this.materials[matGroup]) {
+      return this.materials[matGroup].asArray();
+    }
+    return [];
+  }
+
+  public get Geometry(): Geometry {
+    return this.geometry;
+  }
+
+  public set Geometry(geo: Geometry) {
+    this.geometry = geo;
+  }
+
+  public get Transformer(): Transformer {
+    return this.transformer;
+  }
+
+  public callRecursive(action: Delegates.Action1<SceneObject>) {
+    if (this.children) {
       this.children.forEach(t=> t.callRecursive(action));
-      }
-      action(this);
     }
+    action(this);
+  }
 
-    public onChildrenChanged()
-    {
+  public onChildrenChanged() {
 
-    }
+  }
 
-    public onParentChanged()
-    {
+  public onParentChanged() {
 
-    }
+  }
 
-    public onParentSceneChanged(sceneInfo:IParentSceneChangedEventArgs)
-    {
+  public onParentSceneChanged(sceneInfo: IParentSceneChangedEventArgs) {
 
-    }
+  }
 
-    public update() {
+  public update() {
 
-    }
+  }
 }
 
-export=SceneObject;
+export =SceneObject;
