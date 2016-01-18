7356a6710b4de86d50e13f437ecefd2e57d4facc
LimeStreem <LimeStreem@gmail.com>
2016-01-19 00:34:59 +0900

    :bug: fixed phong material when texture was applied

--- jThree/src/Goml/Nodes/Texture/TextureNodeBase.ts
+++ jThree/src/Goml/Nodes/Texture/TextureNodeBase.ts
@@ -67,7 +67,6 @@ class TextureNodeBase extends GomlTreeNodeBase {
     var rm = JThreeContext.getContextComponent<ResourceManager>(ContextComponents.ResourceManager);
     var name = this.attributes.getValue("name");
     this.targetTexture = this.generateTexture(name, rm);
-    debugger;
     this.nodeManager.nodeRegister.addObject("jthree.resource." + this.TextureGroupName, name, this);
   }
 
