92e2c375217d0b07e49efd19e11a34a7b04c5562
pnlybubbles <pnlybubbles@gmail.com>
2016-01-24 15:49:22 +0900

    feat: add sample code

--- jThree/wwwroot/debug/debugCodes/Interface/Interface.js
+++ jThree/wwwroot/debug/debugCodes/Interface/Interface.js
@@ -1,5 +1,6 @@
 j3(() => {
   const obj0 = j3('#obj0');
+  const obj4 = j3('#obj4');
   console.log(obj0);
   const scene = j3('scene[name=mainScene]');
   console.log(scene);
@@ -8,7 +9,12 @@ j3(() => {
   console.log(cam);
   setTimeout(() => {
     obj0.attr('position', '3,6,-5');
-    console.log(obj0.attr('position'));
+    console.log('obj0', obj0.attr('position'));
   }, 3000);
-  console.log(obj0.attr('position'));
+  console.log('obj0', obj0.attr('position'));
+  setTimeout(() => {
+    obj4.attrObj('position', new j3.Math.Vector3(-3, 6, -5));
+    console.log('obj4', obj4.attrObj('position'));
+  }, 3000);
+  console.log('obj4', obj4.attrObj('position'));
 });
