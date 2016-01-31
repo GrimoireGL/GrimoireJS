3cc5692075b639bebb9c7c31cb0f4bd2370b12c7
LimeStreem <LimeStreem@gmail.com>
2016-01-31 20:26:09 +0900

    :sparkles: Simplified vector parser

--- jThree/src/Math/VectorBase.ts
+++ jThree/src/Math/VectorBase.ts
@@ -1,5 +1,13 @@
 import {GLM} from "gl-matrix";
 import {Func3} from "../Base/Delegates";
+import {InvalidStringException} from "../Exceptions";
+interface IVectorParseDescription {
+  needNormalize: boolean;
+  needNegate: boolean;
+  coefficient: number;
+  elements: number[];
+}
+
 class VectorBase {
 
   public rawElements: GLM.IArray;
@@ -49,6 +57,38 @@ class VectorBase {
     return f;
   }
 
+  protected static __parse(str: string): IVectorParseDescription {
+    const checkRegex = /(-?)([\d,E\+\-\.]+)?(n)?\(([-\d,E\+\.\s]+)\)/g;
+    const matches = checkRegex.exec(str);
+    if (matches) {
+      if (!matches[4]) { // When (x,x,x,x) was not specifed
+        throw new InvalidStringException(`The specified string '${str}' is not containing braced vector.`);
+      }
+      return {
+        needNormalize: matches[3] === "n",
+        needNegate: matches[1] === "-",
+        coefficient:parseFloat(matches[2]),
+        elements: VectorBase.__parseRawVector(matches[4])
+      };
+    } else {
+      // Assume this is simplified format.
+      return {
+        needNormalize: false,
+        needNegate: false,
+        elements: VectorBase.__parseRawVector(str),
+        coefficient: undefined
+      };
+    }
+  }
+
+  private static __parseRawVector(str: string): number[] {
+    const splitted = str.split(",");
+    const result = new Array(splitted.length);
+    for (let i = 0; i < splitted.length; i++) {
+      result[i] = parseFloat(splitted[i]);
+    }
+    return result;
+  }
 }
 
 export default VectorBase;
