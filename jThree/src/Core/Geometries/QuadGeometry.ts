7d8e278bddfb45c8d9d16f96f334be93b3b695dd
moajo <mimirosiasd@gmail.com>
2016-02-11 09:18:07 +0900

    implement divided QuadGeometry.

--- jThree/src/Core/Geometries/QuadGeometry.ts
+++ jThree/src/Core/Geometries/QuadGeometry.ts
@@ -5,27 +5,39 @@ import ContextComponents from "../../ContextComponents";
 import ResourceManager from "../ResourceManager";
 import JThreeContext from "../../JThreeContext";
 class QuadGeometry extends BasicGeometry {
-    constructor(name: string) {
-        super();
-        const rm = JThreeContext.getContextComponent<ResourceManager>(ContextComponents.ResourceManager);
-        this.indexBuffer = rm.createBuffer(name + "index", WebGLRenderingContext.ELEMENT_ARRAY_BUFFER, WebGLRenderingContext.STATIC_DRAW, 1, WebGLRenderingContext.UNSIGNED_BYTE);
-        this.positionBuffer = rm.createBuffer(name + "-pos", WebGLRenderingContext.ARRAY_BUFFER, WebGLRenderingContext.STATIC_DRAW, 3, WebGLRenderingContext.FLOAT);
-        this.normalBuffer = rm.createBuffer(name + "-nor", WebGLRenderingContext.ARRAY_BUFFER, WebGLRenderingContext.STATIC_DRAW, 3, WebGLRenderingContext.FLOAT);
-        this.uvBuffer = rm.createBuffer(name + "-uv", WebGLRenderingContext.ARRAY_BUFFER, WebGLRenderingContext.STATIC_DRAW, 2, WebGLRenderingContext.FLOAT);
-        this.updateBuffers();
-    }
+  constructor(name: string) {
+    super();
+    const rm = JThreeContext.getContextComponent<ResourceManager>(ContextComponents.ResourceManager);
+    this.indexBuffer = rm.createBuffer(name + "index", WebGLRenderingContext.ELEMENT_ARRAY_BUFFER, WebGLRenderingContext.STATIC_DRAW, 1, WebGLRenderingContext.UNSIGNED_BYTE);
+    this.positionBuffer = rm.createBuffer(name + "-pos", WebGLRenderingContext.ARRAY_BUFFER, WebGLRenderingContext.STATIC_DRAW, 3, WebGLRenderingContext.FLOAT);
+    this.normalBuffer = rm.createBuffer(name + "-nor", WebGLRenderingContext.ARRAY_BUFFER, WebGLRenderingContext.STATIC_DRAW, 3, WebGLRenderingContext.FLOAT);
+    this.uvBuffer = rm.createBuffer(name + "-uv", WebGLRenderingContext.ARRAY_BUFFER, WebGLRenderingContext.STATIC_DRAW, 2, WebGLRenderingContext.FLOAT);
+    this.updateBuffers();
+  }
 
-    protected updateBuffers(): void {
-        const pos: number[] = [];
-        const nor: number[] = [];
-        const uv: number[] = [];
-        const index: number[] = [];
-        GeometryBuilder.addQuad(pos, nor, uv, index, [new Vector3(-1, 1, 0), new Vector3(-1, -1, 0), new Vector3(1, 1, 0)]);
-        this.positionBuffer.update(new Float32Array(pos), pos.length);
-        this.normalBuffer.update(new Float32Array(nor), nor.length);
-        this.uvBuffer.update(new Float32Array(uv), uv.length);
-        this.indexBuffer.update(new Uint8Array(index), index.length);
-    }
+  private divX: number = 2;
+  private divY: number = 2;
+
+  public set DivX(num: number) {
+    this.divX = num;
+    this.updateBuffers();
+  }
+  public set DivY(num: number) {
+    this.divX = num;
+    this.updateBuffers();
+  }
+
+  protected updateBuffers(): void {
+    const pos: number[] = [];
+    const nor: number[] = [];
+    const uv: number[] = [];
+    const index: number[] = [];
+    GeometryBuilder.addDividedQuad(pos, nor, uv, index, this.divX, this.divY);
+    this.positionBuffer.update(new Float32Array(pos), pos.length);
+    this.normalBuffer.update(new Float32Array(nor), nor.length);
+    this.uvBuffer.update(new Float32Array(uv), uv.length);
+    this.indexBuffer.update(new Uint8Array(index), index.length);
+  }
 }
 
 export default QuadGeometry;
