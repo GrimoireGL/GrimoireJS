a00fd596d302e552942f314f7ebddbe1a9954b66
case2912 <shiroyama@maia.eonet.ne.jp>
2016-02-22 21:57:03 +0900

    add Quaternion test

--- test/Math/QuaternionTest.js
+++ test/Math/QuaternionTest.js
@@ -3,6 +3,9 @@ const assert = PA.default;
 
 import sinon from "sinon";
 import Quaternion from "../../lib/Math/Quaternion";
+import Vector3 from "../../lib/Math/Vector3";
+import Matrix from "../../lib/Math/Matrix";
+
 
 describe('Quaternion', ()=>{
   it('Quaternion equals',()=>{
@@ -15,5 +18,32 @@ describe('Quaternion', ()=>{
   it('Quaternion multiply',()=>{
     assert(Quaternion.equals(Quaternion.Multiply(new Quaternion([10,10,10,10]),
     new Quaternion([10,10,10,10])),new Quaternion([200,200,200,-200])));
+  });
+  it('To Do Quaternion angleAxis',()=>{
+
+  });
+  it('Quaternion euler',()=>{
+    assert(Quaternion.equals(Quaternion.Euler(0,0,0),new Quaternion([0,0,0,1])));
+  });
+  it('To Do Quaternion eulerXYZ',()=>{
+
+  });
+  it('To Do Quaternion Slerp',()=>{
+
+  });
+  it('To Do Quaternion ',()=>{
+
+  });
+  it('To Do Quaternion Angle',()=>{
+
+  });
+  it('To Do Quaternion FromToRotation',()=>{
+
+  });
+  it('To Do Quaternion LookRotation',()=>{
+
+  });
+  it('To Do Quaternion identity',()=>{
+    Matrix.
   });
 });
