0c1049d7138147d866031d0dcadc367a4128e8c6
moajo <mimirosiasd@gmail.com>
2016-03-03 15:23:09 +0900

    add debug form

--- wwwroot/debug/debugCodes/PMXModel/Sample.js
+++ wwwroot/debug/debugCodes/PMXModel/Sample.js
@@ -1,3 +1,10 @@
 j3(function(){
-
+  var body = document.body;
+  var input = document.createElement("input");
+  input.setAttribute("type","number");
+  input.value = 0;
+  input.addEventListener("change",function(){
+    j3("vmd").attr("frame",input.value + "");
+  });
+  body.appendChild(input);
 });
