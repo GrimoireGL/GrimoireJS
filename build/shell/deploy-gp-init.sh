65b25daa5b19d128d39fd7343b1d651b811a859a
LimeStreem <LimeStreem@gmail.com>
2016-03-10 21:27:25 +0900

    fix for ci

--- build/shell/deploy-gp-init.sh
+++ build/shell/deploy-gp-init.sh
@@ -2,3 +2,5 @@ cd ci
 git init
 git checkout -b gh-pages
 git clone https://github.com/jThreeJS/jThree -b gh-pages
+git remote add origin https://github.com/jThreeJS/jThree
+git pull origin gh-pages
--- build/shell/deploy-gp.sh
+++ build/shell/deploy-gp.sh
@@ -1,5 +1,4 @@
 cd ci
 git add -A
 git commit -m "auto deploy by CircleCI [ci skip]"
-git remote add origin https://github.com/jThreeJS/jThree
 git push origin gh-pages:gh-pages
