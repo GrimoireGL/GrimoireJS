8d0ded267ca7c3e37936d4902a25d495defc4053
LimeStreem <LimeStreem@gmail.com>
2016-03-10 20:19:54 +0900

    fix for ci

--- build/shell/deploy-gp-init.sh
+++ build/shell/deploy-gp-init.sh
@@ -2,6 +2,6 @@ if git rev-parse 2> /dev/null; then
  # git有効
  echo "git already initialized"
 else
- git init
- git clone https://github.com/jThreeJS/jThree -b gh-pages
+ git init ci
+ git clone https://github.com/jThreeJS/jThree -b gh-pages ci
 fi
