9438be334c00419d77f296e582b4cde0619ace74
LimeStreem <LimeStreem@gmail.com>
2016-03-15 02:16:06 +0900

    fix parallelism for ci

--- build/shell/test.sh
+++
@@ -1,9 +0,0 @@
-case $CIRCLE_NODE_INDEX in
-  0)
-    npm run build -- --branch=$CIRCLE_BRANCH
-    npm run lint -- --branch=$CIRCLE_BRANCH;;
-  1)
-    npm run doc -- --branch=$CIRCLE_BRANCH;;
-  2)
-    sh ./build/shell/test.sh ;;
-esac
