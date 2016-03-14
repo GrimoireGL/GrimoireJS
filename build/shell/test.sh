7d8d06dd510d2b611b83fbcf419ecc97fc935593
LimeStreem <LimeStreem@gmail.com>
2016-03-15 01:27:26 +0900

    Separate CI task

---
+++ build/shell/test.sh
@@ -0,0 +1,9 @@
+case $CIRCLE_NODE_INDEX in
+  0)
+    npm run build -- --branch=$CIRCLE_BRANCH
+    npm run lint -- --branch=$CIRCLE_BRANCH;;
+  1)
+    npm run doc -- --branch=$CIRCLE_BRANCH;;
+  2)
+    npm run cover -- --report-dir=ci/cover/$CIRCLE_BRANCH;;
+esac
