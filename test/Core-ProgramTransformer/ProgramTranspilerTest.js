e124f3243af556b2cf631b7a4f0bb939bfd0d4d2
LimeStreem <LimeStreem@gmail.com>
2016-03-17 14:07:45 +0900

    fix test

--- test/Core-ProgramTransformer/ProgramTranspilerTest.js
+++ test/Core-ProgramTransformer/ProgramTranspilerTest.js
@@ -5,7 +5,20 @@ import ProgramTransformer from '../../lib/Core/ProgramTransformer/Transformer/Ba
 ProgramTranspiler.transformerGenerator = function() {
   return [
     new ProgramTransformer((t) => Promise.resolve(t)),
-    new ProgramTransformer((t) => Promise.resolve(t))
+    new ProgramTransformer((t) => Promise.resolve({
+      initialSource: t.initialSource,
+      transformSource: "TEST2 SOURCE",
+      description: {
+        fragment: 0,
+        vertex: 1,
+        uniforms: 2,
+        attributes: 3,
+        fragmentPrecisions: 4,
+        vertexPrecisions: 5,
+        functions: 6
+      }
+    })),
+    new ProgramTransformer((t) => Promise.resolve(t)),
   ];
 }
 test('should transform link completed even if the transformer was empty', async(t) => {
@@ -29,15 +42,15 @@ test('should transform link completed even if the transformer was specified', as
   const result = await ProgramTranspiler.transform("TEST SOURCE", ProgramTranspiler.transformerGenerator());
   t.ok(_.isEqual(result, {
     initialSource: "TEST SOURCE",
-    transformSource: "TEST SOURCE",
+    transformSource: "TEST2 SOURCE",
     description: {
-      fragment: null,
-      vertex: null,
-      uniforms: null,
-      attributes: null,
-      fragmentPrecisions: null,
-      vertexPrecisions: null,
-      functions: null
+      fragment: 0,
+      vertex: 1,
+      uniforms: 2,
+      attributes: 3,
+      fragmentPrecisions: 4,
+      vertexPrecisions: 5,
+      functions: 6
     }
   }));
 });
@@ -45,12 +58,12 @@ test('should transform link completed even if the transformer was specified', as
 test('parseCombined should work', async(t) => {
   const result = await ProgramTranspiler.parseCombined("TEST SOURCE");
   t.ok(_.isEqual(result, {
-      fragment: null,
-      vertex: null,
-      uniforms: null,
-      attributes: null,
-      fragmentPrecisions: null,
-      vertexPrecisions: null,
-      functions: null
+      fragment: 0,
+      vertex: 1,
+      uniforms: 2,
+      attributes: 3,
+      fragmentPrecisions: 4,
+      vertexPrecisions: 5,
+      functions: 6
     }));
 });
