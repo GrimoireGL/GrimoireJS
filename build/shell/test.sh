8ce27beb139c9e964ace50b2f083a8629f78e02e
LimeStreem <LimeStreem@gmail.com>
2016-03-19 03:09:57 +0900

    add codecov

--- build/shell/test.sh
+++ build/shell/test.sh
@@ -5,11 +5,11 @@ case $CIRCLE_NODE_INDEX in
   1)
     npm run doc -- --branch=$CIRCLE_BRANCH;;
   2)
-    npm run cover -- --report-dir=ci/cover/$CIRCLE_BRANCH
+    npm run cover -- --report-dir=ci/cover/$CIRCLE_BRANCH && /node_modules/.bin/codecov
     rc=$?;
     if [ $rc != 0 ]; then
       npm rebuild
-      npm run cover -- --report-dir=ci/cover/$CIRCLE_BRANCH
+      npm run cover -- --report-dir=ci/cover/$CIRCLE_BRANCH && /node_modules/.bin/codecov
       exit $?
     else
       exit $rc
