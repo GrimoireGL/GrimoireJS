034cbcdbc56be5a2883bbd400f29a990879dbf0c
LimeStreem <LimeStreem@gmail.com>
2016-03-19 11:20:25 +0900

    add cover all

--- build/shell/test.sh
+++ build/shell/test.sh
@@ -5,11 +5,11 @@ case $CIRCLE_NODE_INDEX in
   1)
     npm run doc -- --branch=$CIRCLE_BRANCH;;
   2)
-    npm run cover -- --report-dir=ci/cover/$CIRCLE_BRANCH && /node_modules/.bin/codecov
+    npm run cover -- --reporter=text-lcov | coveralls
     rc=$?;
     if [ $rc != 0 ]; then
       npm rebuild
-      npm run cover -- --report-dir=ci/cover/$CIRCLE_BRANCH && /node_modules/.bin/codecov
+      npm run cover -- --reporter=text-lcov | coveralls
       exit $?
     else
       exit $rc
