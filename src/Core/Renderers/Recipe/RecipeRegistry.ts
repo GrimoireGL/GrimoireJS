23d99f098795f8b2d6e42c16d7f15e86d1526a01
LimeStreem <LimeStreem@gmail.com>
2016-06-08 20:13:57 +0900

    fix for test

--- src/Core/Renderers/Recipe/RecipeRegistry.ts
+++ src/Core/Renderers/Recipe/RecipeRegistry.ts
@@ -1,15 +1,23 @@
 import NamedValue from "../../../Base/NamedValue";
 class RecipeRegistry {
-    private static _recipes: NamedValue<string> = {
-        default: require("./BuiltIn/DefaultRecipe.xml")
-    };
+    private static _recipes: NamedValue<string>;
 
     public static appendRecipe(name: string, source: string): void {
+        RecipeRegistry._checkInitialized();
         RecipeRegistry._recipes[name] = source;
     }
 
     public static getRecipe(name: string): string {
+        RecipeRegistry._checkInitialized();
         return RecipeRegistry._recipes[name];
+    }
+
+    private static _checkInitialized(): void {
+        if (!RecipeRegistry._recipes) {
+            RecipeRegistry._recipes = {
+                default: require("./BuiltIn/DefaultRecipe.xml")
+            };
+        }
     }
 }
 
