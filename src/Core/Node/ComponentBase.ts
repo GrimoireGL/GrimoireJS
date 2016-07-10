d81875b7af79d35bae5232784d91e9509a493791
moajo <mimirosiasd@gmail.com>
2016-07-11 00:44:22 +0900

    add ComponentBase.OptionalAttributes

--- src/Core/Node/ComponentBase.ts
+++ src/Core/Node/ComponentBase.ts
@@ -2,6 +2,7 @@ import GomlAttribute from "./GomlAttribute";
 
 abstract class ComponentBase {
   public abstract get RequiredAttributes(): GomlAttribute[];
+  public abstract get OptionalAttributes(): GomlAttribute[];
 }
 
 export default ComponentBase;
