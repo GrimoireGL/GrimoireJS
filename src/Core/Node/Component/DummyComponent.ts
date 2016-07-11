2ef8198050c6e2c33abdc78356e13fc0dfe3d4bd
moajo <mimirosiasd@gmail.com>
2016-07-11 16:36:46 +0900

    implement Namespace to componentBase

--- src/Core/Node/Component/DummyComponent.ts
+++ src/Core/Node/Component/DummyComponent.ts
@@ -2,13 +2,16 @@ import ComponentBase from "../ComponentBase";
 import GomlAttribute from "../GomlAttribute";
 
 class DummyComponent extends ComponentBase {
+  public get Namespace(): string {
+    return "http://aaaa";
+  }
   public get RequiredAttributes(): GomlAttribute[] {
-    let a = new GomlAttribute("name1", 123);
+    let a = new GomlAttribute("name1", this.Namespace, 123);
     return [a];
   }
 
   public get OptionalAttributes(): GomlAttribute[] {
-    let a = new GomlAttribute("optional1", 999);
+    let a = new GomlAttribute("optional1", this.Namespace, 999);
     return [a];
   }
 
--- src/Core/Node/ComponentBase.ts
+++ src/Core/Node/ComponentBase.ts
@@ -1,6 +1,7 @@
 import GomlAttribute from "./GomlAttribute";
 
 abstract class ComponentBase {
+  public abstract get Namespace(): string;
   public abstract get RequiredAttributes(): GomlAttribute[];
   public abstract get OptionalAttributes(): GomlAttribute[];
 }
