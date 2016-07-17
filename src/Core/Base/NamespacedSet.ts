1390f3aab03543f270afd64e78b9aab8ac16a40c
moajo <mimirosiasd@gmail.com>
2016-07-17 22:58:21 +0900

    fix NamespacedSet, implement toArray();

--- src/Core/Base/NamespacedSet.ts
+++ src/Core/Base/NamespacedSet.ts
@@ -1,42 +1,53 @@
 import NamespacedIdentity from "./NamespacedIdentity";
 
 class NamespacedSet {
-    /**
-     * The map to containing all values managed by fqn.
-     * @type {Map<string, V>}
-     */
-    private _fqnObjectMap: Map<string, NamespacedIdentity> = new Map<string, NamespacedIdentity>();
+  /**
+   * The map to containing all values managed by fqn.
+   * @type {Map<string, V>}
+   */
+  private _fqnObjectMap: Map<string, NamespacedIdentity> = new Map<string, NamespacedIdentity>();
 
-    public push(item: NamespacedIdentity): void {
-        this._fqnObjectMap.set(item.fqn, item);
-    }
+  public push(item: NamespacedIdentity): NamespacedSet {
+    this._fqnObjectMap.set(item.fqn, item);
+    return this;
+  }
 
-    public pushArray(item: NamespacedIdentity[]): void {
-        item.forEach(v => {
-            this.push(v);
-        });
-    }
+  public pushArray(item: NamespacedIdentity[]): NamespacedSet {
+    item.forEach(v => {
+      this.push(v);
+    });
+    return this;
+  }
 
-    public values(): IterableIterator<NamespacedIdentity> {
-        return this._fqnObjectMap.values();
-    }
+  public values(): IterableIterator<NamespacedIdentity> {
+    return this._fqnObjectMap.values();
+  }
 
-    public clone(): NamespacedSet {
-        const newSet = new NamespacedSet();
-        const values = this.values();
-        for (let i of values) {
-         newSet.push(i);
-        }
-        return newSet;
+  public toArray(): NamespacedIdentity[] {
+    const ret: NamespacedIdentity[] = [];
+    const values = this.values();
+    for (let val of values) {
+      ret.push(val);
     }
+    return ret;
+  }
 
-    public merge(other): NamespacedSet {
-      const values = other.values();
-      for (let elem of values) {
-       this.push(elem);
-      }
-      return this;
+  public clone(): NamespacedSet {
+    const newSet = new NamespacedSet();
+    const values = this.values();
+    for (let i of values) {
+      newSet.push(i);
     }
+    return newSet;
+  }
+
+  public merge(other): NamespacedSet {
+    const values = other.values();
+    for (let elem of values) {
+      this.push(elem);
+    }
+    return this;
+  }
 }
 
 export default NamespacedSet;
