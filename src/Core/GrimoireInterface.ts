576ad0e06ac1be2b527f178fc066ad418bf7c994
moajo <mimirosiasd@gmail.com>
2016-07-24 21:44:06 +0900

    fix ensureTobeComponentConstructor

--- src/Core/GrimoireInterface.ts
+++ src/Core/GrimoireInterface.ts
@@ -12,90 +12,92 @@ import Ensure from "./Base/Ensure";
 import {inherits} from "util";
 
 interface IGrimoireInterfaceBase {
-    ns(ns: string): (name: string) => NamespacedIdentity;
+  ns(ns: string): (name: string) => NamespacedIdentity;
 
 }
 
 interface IGrimoireInterface extends IGrimoireInterfaceBase {
-    (query: string): GOMLInterface;
+  (query: string): GOMLInterface;
 }
 
 class GrimoireInterfaceImpl implements IGrimoireInterfaceBase {
-    public nodeDeclarations: NamespacedDictionary<NodeDeclaration> = new NamespacedDictionary<NodeDeclaration>();
+  public nodeDeclarations: NamespacedDictionary<NodeDeclaration> = new NamespacedDictionary<NodeDeclaration>();
 
-    public converters: NamespacedDictionary<AttributeConverter> = new NamespacedDictionary<AttributeConverter>();
+  public converters: NamespacedDictionary<AttributeConverter> = new NamespacedDictionary<AttributeConverter>();
 
-    public componentDeclarations: NamespacedDictionary<ComponentDeclaration> = new NamespacedDictionary<ComponentDeclaration>();
+  public componentDeclarations: NamespacedDictionary<ComponentDeclaration> = new NamespacedDictionary<ComponentDeclaration>();
 
-    public loadTasks: (() => Promise<void>)[] = [];
-    /**
-     * Generate namespace helper function
-     * @param  {string} ns namespace URI to be used
-     * @return {[type]}    the namespaced identity
-     */
-    public ns(ns: string): (name: string) => NamespacedIdentity {
-        return (name: string) => new NamespacedIdentity(ns, name);
-    }
+  public loadTasks: (() => Promise<void>)[] = [];
+  /**
+   * Generate namespace helper function
+   * @param  {string} ns namespace URI to be used
+   * @return {[type]}    the namespaced identity
+   */
+  public ns(ns: string): (name: string) => NamespacedIdentity {
+    return (name: string) => new NamespacedIdentity(ns, name);
+  }
 
-    public register(loadTask: () => Promise<void>): void {
-        this.loadTasks.push(loadTask);
-    }
+  public register(loadTask: () => Promise<void>): void {
+    this.loadTasks.push(loadTask);
+  }
 
-    // TODO test
-    public registerComponent(name: string | NamespacedIdentity, attributes: { [name: string]: IAttributeDeclaration }, obj: Object | (new () => Component)): void {
-        name = Ensure.ensureTobeNamespacedIdentity(name);
-        obj = this._ensureTobeComponentConstructor(obj);
-        this.componentDeclarations.set(name as NamespacedIdentity, new ComponentDeclaration(name as NamespacedIdentity, attributes, obj as (new () => Component)));
-    }
+  // TODO test
+  public registerComponent(name: string | NamespacedIdentity, attributes: { [name: string]: IAttributeDeclaration }, obj: Object | (new () => Component)): void {
+    name = Ensure.ensureTobeNamespacedIdentity(name);
+    obj = this._ensureTobeComponentConstructor(obj);
+    this.componentDeclarations.set(name as NamespacedIdentity, new ComponentDeclaration(name as NamespacedIdentity, attributes, obj as (new () => Component)));
+  }
   public registerComponentDec(declaration: ComponentDeclaration): void {
     this.componentDeclarations.set(declaration.name, declaration);
   }
 
-    public registerNode(name: string | NamespacedIdentity,
-        requiredComponents: (string | NamespacedIdentity)[],
-        defaultValues?: { [key: string]: any } | NamespacedDictionary<any>,
-        superNode?: string | NamespacedIdentity,
-        requiredComponentsForChildren?: (string | NamespacedIdentity)[]): void {
-        name = Ensure.ensureTobeNamespacedIdentity(name);
-        requiredComponents = Ensure.ensureTobeNamespacedIdentityArray(requiredComponents);
-        defaultValues = Ensure.ensureTobeNamespacedDictionary<any>(defaultValues, (name as NamespacedIdentity).ns);
-        superNode = Ensure.ensureTobeNamespacedIdentity(superNode);
-        requiredComponentsForChildren = Ensure.ensureTobeNamespacedIdentityArray(requiredComponentsForChildren);
-        this.nodeDeclarations.set(name as NamespacedIdentity,
-            new NodeDeclaration(name as NamespacedIdentity,
-                NamespacedSet.fromArray(requiredComponents as NamespacedIdentity[]),
-                defaultValues as NamespacedDictionary<any>,
-                superNode as NamespacedIdentity,
-                NamespacedSet.fromArray(requiredComponentsForChildren as NamespacedIdentity[])
-            )
-        );
-    }
+  public registerNode(name: string | NamespacedIdentity,
+    requiredComponents: (string | NamespacedIdentity)[],
+    defaultValues?: { [key: string]: any } | NamespacedDictionary<any>,
+    superNode?: string | NamespacedIdentity,
+    requiredComponentsForChildren?: (string | NamespacedIdentity)[]): void {
+    name = Ensure.ensureTobeNamespacedIdentity(name);
+    requiredComponents = Ensure.ensureTobeNamespacedIdentityArray(requiredComponents);
+    defaultValues = Ensure.ensureTobeNamespacedDictionary<any>(defaultValues, (name as NamespacedIdentity).ns);
+    superNode = Ensure.ensureTobeNamespacedIdentity(superNode);
+    requiredComponentsForChildren = Ensure.ensureTobeNamespacedIdentityArray(requiredComponentsForChildren);
+    this.nodeDeclarations.set(name as NamespacedIdentity,
+      new NodeDeclaration(name as NamespacedIdentity,
+        NamespacedSet.fromArray(requiredComponents as NamespacedIdentity[]),
+        defaultValues as NamespacedDictionary< any >,
+        superNode as NamespacedIdentity,
+        NamespacedSet.fromArray(requiredComponentsForChildren as NamespacedIdentity[])
+        )
+      );
+  }
   public registerNodeDec(declaration: NodeDeclaration): void {
     this.nodeDeclarations.set(declaration.name, declaration);
   }
 
-    public registerConverter(name: string | NamespacedIdentity, converter: (any) => any): void {
-        name = Ensure.ensureTobeNamespacedIdentity(name);
-        this.converters.set(name as NamespacedIdentity, { name: name as NamespacedIdentity, convert: converter });
-    }
+  public registerConverter(name: string | NamespacedIdentity, converter: (any) => any): void {
+    name = Ensure.ensureTobeNamespacedIdentity(name);
+    this.converters.set(name as NamespacedIdentity, { name: name as NamespacedIdentity, convert: converter });
+  }
 
-    private _ensureTobeComponentConstructor(obj: Object | (new () => Component)): new () => Component {
-        if (typeof obj === "function") {
-            if (!((obj as Function).prototype instanceof Component)) {
-                throw new Error("Component constructor must extends Component class.");
-            }
-        } else if (typeof obj === "object") {
-            const newCtor = () => {
-                return this;
-            };
-            for (let key in obj) {
-                (newCtor as Function).prototype[key] = obj[key];
-            }
-            inherits(newCtor, Component);
-            obj = newCtor;
-        }
-        return obj as (new () => Component);
+  private _ensureTobeComponentConstructor(obj: Object | (new () => Component)): new () => Component {
+    if (typeof obj === "function") {
+      if (!((obj as Function).prototype instanceof Component)) {
+        throw new Error("Component constructor must extends Component class.");
+      }
+    } else if (typeof obj === "object") {
+      const newCtor = () => {
+        return this;
+      };
+      for (let key in obj) {
+        (newCtor as Function).prototype[key] = obj[key];
+      }
+      inherits(newCtor, Component);
+      obj = newCtor;
+    } else if (!obj) {
+      obj = Component;
     }
+    return obj as (new () => Component);
+  }
 }
 
 // TODO export as IGrimoireInterface
