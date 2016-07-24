2f003e9353374077af86f44ca8ff9bc0131cf55f
moajo <mimirosiasd@gmail.com>
2016-07-24 16:53:29 +0900

    rename and fix sample Node

---
+++ src/Core/Node/Nodes/GomlNodeDeclaration.ts
@@ -0,0 +1,23 @@
+import NamespacedDictionary from "../../Base/NamespacedDictionary";
+import NamespacedSet from "../../Base/NamespacedSet";
+import NamespacedIdentity from "../../Base/NamespacedIdentity";
+import NodeDeclaration from "../NodeDeclaration";
+import Constants from "../../Base/Constants";
+
+class GomlNodeDeclaration extends NodeDeclaration {
+
+  constructor() {
+    const id = new NamespacedIdentity(Constants.defaultNamespace, "GOML");
+    const req = new NamespacedSet();
+    req.push(new NamespacedIdentity("Dummy"));
+    console.log("aaaaaaaa:"+JSON.stringify(req));
+    const defaultValueDict = new NamespacedDictionary<any>();
+    defaultValueDict.set(new NamespacedIdentity("aaa"), 100);
+    const reqForChildren = new NamespacedSet();
+    reqForChildren.push(new NamespacedIdentity("Dummy"));
+    super(id, req, defaultValueDict, null, reqForChildren);
+  }
+}
+
+
+export default GomlNodeDeclaration;
