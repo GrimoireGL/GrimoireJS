4f453c80195353204975c7c1e47da0301f162177
moajo <mimirosiasd@gmail.com>
2016-07-24 16:47:24 +0900

    add gomlParserTest

--- test/Node/GomlParserTest.js
+++ test/Node/GomlParserTest.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 import GomlParser from "../../lib-es5/Core/Node/GomlParser";
 import xmldom from 'xmldom';
+import DefaultPluginRegister from "../../lib-es5/Core/Node/DefaultPluginRegister"
 
 function loadFromTestResource(path){
   return require("./_TestResource/"+path);
@@ -12,7 +13,10 @@ function obtainElementTag(path){
   return parser.parseFromString(loadFromTestResource(path),"text/xml").documentElement;
 }
 
+DefaultPluginRegister.register();
+
 test('test parse for goml parser',(t)=>{
   const element = obtainElementTag("GOMLParserTest_Case1.goml");
+  GomlParser.parse(element);
   console.log(element);
 });
