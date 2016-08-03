f08752a66c9eba4ad69f264883251e89ee32aa66
moajo <mimirosiasd@gmail.com>
2016-08-03 14:55:14 +0900

    implement NodeInterface

--- src/Core/Interface/NodeInterface.ts
+++ src/Core/Interface/NodeInterface.ts
@@ -1,3 +1,7 @@
+import XMLReader from "../Base/XMLReader";
+import GomlParser from "../Node/GomlParser";
+import Attribute from "../Node/Attribute";
+import NamespacedIdentity from "../Base/NamespacedIdentity";
 import Component from "../Node/Component";
 import INodeInterfaceBase from "./INodeInterfaceBase";
 import ComponentInterface from "./ComponentInterface";
@@ -11,11 +15,69 @@ class NodeInterface implements INodeInterfaceBase {
 
   }
   public queryFunc(query: string): IComponentInterface {
-    return  new ComponentInterface(this.queryComponents(query));
+    return new ComponentInterface(this.queryComponents(query));
   }
 
-  public queryComponents(query: string): Component[][][]{
+  public queryComponents(query: string): Component[][][] {
     return null; // TODO: implement!
+  }
+
+  public attr(attrName: string|NamespacedIdentity): Attribute;
+  public attr(attrName: string|NamespacedIdentity, value: any): void;
+  public attr(attrName: string|NamespacedIdentity, value?: any): Attribute|void {
+    if (value === void 0) {
+      // return Attribute.
+      this.forEach((node) => {
+        const attr = node.attributes.get(attrName as string);
+        if (!attr) {
+          return attr;
+        }
+      });
+    } else {
+      // set value.
+      this.forEach((node) => {
+        const attr = node.attributes.get(attrName as string);
+        if (!attr) {
+          attr.Value = value;
+        }
+      });
+    }
+  }
+  public on(eventName: string, listener: Function): void {
+    this.forEach((node) => {
+      node.on(eventName, listener);
+    });
+  }
+  public off(eventName: string, listener: Function): void {
+    this.forEach((node) => {
+      node.removeListener(eventName, listener);
+    })
+  }
+  public append(tag: string): void {
+    this.forEach((node) => {
+      const elems = XMLReader.parseXML(tag);
+      const nodes = elems.map((elem) => GomlParser.parse(elem));
+      nodes.forEach((child) => {
+        node.addChild(child);
+      });
+    })
+  }
+  public remove(child: GomlNode): void {
+    this.forEach((node) => {
+      node.removeChild(child);
+    });
+  }
+  public forEach(callback: ((node: GomlNode) => void)): void {
+    this.nodes.forEach((array) => {
+      array.forEach((node) => {
+        callback(node);
+      });
+    });
+  }
+  public setEnable(enable: boolean): void {
+    this.forEach((node) => {
+      node.enable = !!enable;
+    });
   }
 }
 
