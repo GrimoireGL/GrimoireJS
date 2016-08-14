fee8c0fd5b8509aff12337449ebac96b45ff1b7a
LimeStreem <LimeStreem@gmail.com>
2016-08-14 15:44:20 +0900

    implemented component interfaces

--- src/Core/Interface/IGomlInterfaceBase.ts
+++ src/Core/Interface/IGomlInterfaceBase.ts
@@ -1,5 +1,10 @@
+import Component from "../Node/Component";
 interface IGomlInterfaceBase {
-
+  get<T>(): T;
+  get<T>(componentIndex: number): T;
+  get<T>(nodeIndex: number, componentIndex: number): T;
+  get<T>(treeIndex: number, nodeIndex: number, componentIndex: number): T;
+  forEach(f: (v: Component, compIndex: number, nodeIndex: number, treeIndex: number) => void);
 }
 
 export default IGomlInterfaceBase;
