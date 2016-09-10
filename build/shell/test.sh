5d455da3c98a9839c2c5bd0a934790fe83a989ae
LimeStreem <LimeStreem@gmail.com>
2016-09-10 14:35:05 +0900

    fixed ci task

--- build/shell/test.sh
+++ build/shell/test.sh
@@ -2,10 +2,9 @@ npm install typescript@beta
 case $CIRCLE_NODE_INDEX in
   0)
     npm run compiler-version
-    npm run build -- --branch=$CIRCLE_BRANCH
-    npm run lint -- --branch=$CIRCLE_BRANCH;;
+    npm run build -- --branch=$CIRCLE_BRANCH;;
   1)
-    npm run doc -- --branch=$CIRCLE_BRANCH;;
+    npm run lint -- --branch=$CIRCLE_BRANCH;;
   2)
-    npm run cover -- --branch=$CIRCLE_BRANCH;;
+    npm test -- --branch=$CIRCLE_BRANCH;;
 esac
