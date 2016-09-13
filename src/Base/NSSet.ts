52956a5b4fc0c895d74d727ffb23443ab6c09b73
moajo <mimirosiasd@gmail.com>
2016-09-14 00:32:41 +0900

    NSSet to be keep order

--- src/Base/NSSet.ts
+++ src/Base/NSSet.ts
@@ -1,59 +1,55 @@
 import NSIdentity from "./NSIdentity";
 
 class NSSet {
-    /**
-     * The map to containing all values managed by fqn.
-     * @type {Map<string, V>}
-     */
-    private _fqnObjectMap: Map<string, NSIdentity> = new Map<string, NSIdentity>();
+  private _contentArray: NSIdentity[] = [];
 
-    public static fromArray(array: NSIdentity[]): NSSet {
-        const nSet = new NSSet();
-        nSet.pushArray(array);
-        return nSet;
-    }
+  public static fromArray(array: NSIdentity[]): NSSet {
+    const nSet = new NSSet();
+    nSet.pushArray(array);
+    return nSet;
+  }
 
-    public push(item: NSIdentity): NSSet {
-        this._fqnObjectMap.set(item.fqn, item);
-        return this;
+  public push(item: NSIdentity): NSSet {
+    const index = this._contentArray.findIndex(id => id.fqn === item.fqn);
+    if (index === -1) {
+      this._contentArray.push(item);
     }
+    return this;
+  }
 
-    public pushArray(item: NSIdentity[]): NSSet {
-        item.forEach(v => {
-            this.push(v);
-        });
-        return this;
-    }
+  public pushArray(item: NSIdentity[]): NSSet {
+    item.forEach(v => {
+      this.push(v);
+    });
+    return this;
+  }
 
-    public values(): IterableIterator<NSIdentity> {
-        return this._fqnObjectMap.values();
-    }
+  public values(): IterableIterator<NSIdentity> {
+    return this._contentArray.values();
+  }
 
-    public toArray(): NSIdentity[] {
-        const ret: NSIdentity[] = [];
-        const values = this.values();
-        for (let val of values) {
-            ret.push(val);
-        }
-        return ret;
+  public toArray(): NSIdentity[] {
+    const ret: NSIdentity[] = [];
+    for (let item of this._contentArray) {
+      ret.push(item);
     }
+    return ret;
+  }
 
-    public clone(): NSSet {
-        const newSet = new NSSet();
-        const values = this.values();
-        for (let i of values) {
-            newSet.push(i);
-        }
-        return newSet;
+  public clone(): NSSet {
+    const newSet = new NSSet();
+    for (let i of this._contentArray) {
+      newSet.push(i);
     }
+    return newSet;
+  }
 
-    public merge(other: NSSet): NSSet {
-        const values = other.values();
-        for (let elem of values) {
-            this.push(elem);
-        }
-        return this;
+  public merge(other: NSSet): NSSet {
+    for (let elem of other._contentArray) {
+      this.push(elem);
     }
+    return this;
+  }
 }
 
 export default NSSet;
