e13bda160f054e1e5d2c29bbaf08f42ce67d3b2f
LimeStreem <LimeStreem@gmail.com>
2016-09-23 22:24:20 +0900

    fixed test fail of string converter

--- test/Converters/StringConverterTest.js
+++ test/Converters/StringConverterTest.js
@@ -1,4 +1,5 @@
-import StringConveter from "../../lib-es5/Converters/StringConverter";
+import test from "ava";
+import StringConverter from "../../lib-es5/Converters/StringConverter";
 
 test('StringConverter should convert collectly',(t)=>{
   t.truthy(StringConverter("HELLO") === "HELLO" );
