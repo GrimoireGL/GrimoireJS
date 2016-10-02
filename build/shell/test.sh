a9fb91d1d7b19f533c42577819f14edb92a922d7
LimeStreem <LimeStreem@gmail.com>
2016-10-02 14:36:50 +0900

    Fixed ci configuration

--- build/shell/test.sh
+++ build/shell/test.sh
@@ -1,10 +1,7 @@
 npm install typescript@beta
 case $CIRCLE_NODE_INDEX in
   0)
-    npm run compiler-version
     npm run build -- --branch=$CIRCLE_BRANCH;;
   1)
-    npm run lint -- --branch=$CIRCLE_BRANCH;;
-  2)
     npm test -- --branch=$CIRCLE_BRANCH;;
 esac
