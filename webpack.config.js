6b1f414af20ab54dc00ada3be651a351e59e222a
LimeStreem <LimeStreem@gmail.com>
2016-11-17 10:51:49 +0900

    Improved build task

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,40 @@ const path = require("path");
 const webpack = require("webpack");
 const shell = require("webpack-shell-plugin");
 const argv = require("yargs").argv;
+const fs = require("fs");
+const fnPrefix = JSON.parse(fs.readFileSync(path.resolve(__dirname, "package.json"), "utf-8")).name;
 
-const plugins = [new shell({
-  onBuildStart: "npm run generate-expose",
-  onBuildEnd: "npm run generate-reference"
-})];
-let fileName = "/register/index.js";
+const getBuildTask = (fileName, plugins) => {
+  return {
+    cache: true,
+    entry: path.resolve(__dirname, "src/index.ts"),
+    output: {
+      path: __dirname,
+      filename: "/register/" + fileName,
+      libraryTarget: "umd"
+    },
+    module: {
+      loaders: [{
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "babel-loader?presets[]=es2015,presets[]=stage-2,plugins[]=transform-runtime!ts-loader"
+      }]
+    },
+    resolve: {
+      extensions: ['', '.ts', '.js']
+    },
+    plugins: [new shell({
+      onBuildStart: "npm run generate-expose",
+      onBuildEnd: "npm run generate-reference"
+    })].concat(plugins),
+    devtool: 'source-map'
+  }
+};
+const buildTasks = [getBuildTask(fnPrefix + ".js", [])]
 
 if (argv.prod) {
-  plugins.push(
+  buildTasks.push(getBuildTask("index.js", [])); // for npm registeirng
+  buildTasks.push(getBuildTask(fnPrefix + ".min.js", [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -18,28 +43,8 @@ if (argv.prod) {
       }
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin());
-  fileName = "/register/index.min.js";
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]));
 }
 
-module.exports = {
-  cache: true,
-  entry: path.resolve(__dirname, "src/index.ts"),
-  output: {
-    path: __dirname,
-    filename: fileName,
-    libraryTarget: "umd"
-  },
-  module: {
-    loaders: [{
-      test: /\.ts$/,
-      exclude: /node_modules/,
-      loader: "babel-loader?presets[]=es2015,presets[]=stage-2,plugins[]=transform-runtime!ts-loader"
-    }]
-  },
-  resolve: {
-    extensions: ['', '.ts', '.js']
-  },
-  plugins: plugins,
-  devtool: 'source-map'
-};
+module.exports = buildTasks;
