a0926db9bbc5bf30d044d846e3756b87efcaa510
moajo <mimirosiasd@gmail.com>
2016-12-20 14:19:14 +0900

    add util in utility

--- src/Base/Utility.ts
+++ src/Base/Utility.ts
@@ -13,4 +13,34 @@ export default class Utility {
   public static isSnakeCase(str: string): boolean {
     return /^[a-z0-9\-]+$/.test(str);
   }
+  public static flat<T>(array: T[][]): T[] {
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+      count += array[i].length;
+    }
+    const ret = new Array<T>(count);
+    count = 0;
+    for (let i = 0; i < array.length; i++) {
+      const ar = array[i];
+      for (let j = 0; j < ar.length; j++) {
+        ret[count] = ar[j];
+        count++;
+      }
+    }
+    return ret;
+  }
+  public static flatMap<T>(source: T[], map: (a: T) => T[]): T[] {
+    const c = new Array<T[]>(source.length);
+    for (let i = 0; i < source.length; i++) {
+      c[i] = map(source[i]);
+    }
+    return Utility.flat(c);
+  }
+  public static sum(array: number[]): number {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total;
+  }
 }
