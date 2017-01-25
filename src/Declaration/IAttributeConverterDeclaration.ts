d842a852abdd1e4976ae64f8b72b5cf5a7ba2cd0
moajo <mimirosiasd@gmail.com>
2017-01-25 15:04:21 +0900

    rename

--- src/Declaration/IAttributeConverterDeclaration.ts
+++ src/Declaration/IAttributeConverterDeclaration.ts
@@ -3,8 +3,8 @@ import NSIdentity from "../Base/NSIdentity";
 
 interface IAttributeConverterDeclaration {
   name: string | NSIdentity;
-  parameter: any;
-  verify(attr: Attribute): boolean;
+  [params: string]: any;
+  verify(attr: Attribute): void; // throw error if attribute is not satisfy condition converter needed.
   convert(val: any, attr: Attribute): any;
 }
 export default IAttributeConverterDeclaration;
