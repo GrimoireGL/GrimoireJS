2c587220aaa2407b8908ea22ff90b216f7a192df
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-04-02 21:27:55 +0900

    fix(ci): add document generate script

--- release.sh
+++ release.sh
@@ -2,5 +2,9 @@ npm run semantic-release
 if [ $? = "0" ]; then
   echo PRESET_UPDATE
   curl "https://preset-updater.herokuapp.com/?repo=preset-basic&repoURL=https://github.com/GrimoireGL/grimoirejs-preset-basic.git&buildNumber=$CIRCLE_BUILD_NUM&currentPkg=grimoirejs"
+  echo DOCUMENT_GENERATING
+  npm run doc
+  echo DOCUMENT_UPLOADING
+  aws s3 cp ./docs/ s3://circleci-grimoire-e2e/docs/$CIRCLE_PROJECT_REPONAME --recursive
 fi
 exit 0
