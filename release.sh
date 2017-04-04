bb0259ccb02ca6e3ae3825dc80062647de20e504
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-04-04 17:00:57 +0900

    chore(ci): Fixed deployment script

--- release.sh
+++ release.sh
@@ -2,9 +2,5 @@ npm run semantic-release
 if [ $? = "0" ]; then
   echo PRESET_UPDATE
   curl "https://preset-updater.herokuapp.com/?repo=preset-basic&repoURL=https://github.com/GrimoireGL/grimoirejs-preset-basic.git&buildNumber=$CIRCLE_BUILD_NUM&currentPkg=grimoirejs"
-  echo DOCUMENT_GENERATING
-  npm run doc
-  echo DOCUMENT_UPLOADING
-  aws s3 cp ./docs/ s3://grimoire-api-document/$CIRCLE_PROJECT_REPONAME --recursive --acl public-read
 fi
 exit 0
