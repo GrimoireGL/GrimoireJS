5b94795f40c8da0a2d497746d49c9ae89fef98fc
moajo <mimirosiasd@gmail.com>
2017-05-28 14:27:12 +0900

    fix: build script on webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,42 @@
 const path = require("path");
 const webpack = require("webpack");
-const argv = require("yargs").argv;
 const fs = require("fs");
+
 // Shot package name will be used for prefix of filename
 const fnPrefix = JSON.parse(fs.readFileSync(path.resolve(__dirname, "package.json"), "utf-8")).name.replace("grimoirejs", "grimoire");
 
 // Copy task for optimization
-class CopyPlugin{
-  constructor(copyFrom,copyTo){
+class CopyPlugin {
+  constructor(copyFrom, copyTo) {
     this.copyFrom = copyFrom;
     this.copyTo = copyTo;
   }
-  apply(compiler){
-    compiler.plugin("after-emit",(compiler,callback)=>{
+  apply(compiler) {
+    compiler.plugin("after-emit", (compiler, callback) => {
       fs.createReadStream(this.copyFrom).pipe(fs.createWriteStream(this.copyTo));
       callback();
+    });
+  }
+}
+
+class CauldronPlugin {
+  constructor() {}
+  apply(compiler) {
+    compiler.plugin("compile", () => {
+      try {
+        console.log(require('child_process').execSync('npm run generate-expose').toString());
+      } catch (e) {
+        console.log(e);
+      }
+    });
+    compiler.plugin('after-emit', (compiler, callback) => {
+      try {
+        console.log(require('child_process').execSync('npm run generate-reference').toString());
+      } catch (e) {
+        console.log(e);
+      } finally {
+        callback();
+      }
     });
   }
 }
@@ -43,31 +65,33 @@ const getBuildTask = (fileName, plugins, needPolyfill) => {
   }
 };
 
-module.exports = (env)=>{
+module.exports = (env) => {
   let includeCore = true;
   // if this package was preset including core or core package,
   // Script for browser needs babel-polyfill
-  let polyfills = includeCore ? [true,false,true] : [false,false,false];
+  let polyfills = includeCore ? [true, false, true] : [false, false, false];
   env = env || {};
   let buildTasks = [];
   let isDefault = !env.browser && !env.npm && !env.prod;
   let skipIndex = false;
-  if(env.browser || env.prod){
-    const plugins = [];
+  let cauldron = new CauldronPlugin();
+  if (env.browser || env.prod) {
+    const plugins = [cauldron];
     // if needs index also and it was not including core, index.js must be copied from fnPrefix.js
     // since these are completely same build task. Yeah, optimization.
-    if(!includeCore && (env.prod || env.npm)){
-      plugins.push(new CopyPlugin(`./register/${fnPrefix}.js`,'./register/index.js'));
-      plugins.push(new CopyPlugin(`./register/${fnPrefix}.js.map`,'./register/index.js.map'));
+    if (!includeCore && (env.prod || env.npm)) {
+      plugins.push(new CopyPlugin(`./register/${fnPrefix}.js`, './register/index.js'));
+      plugins.push(new CopyPlugin(`./register/${fnPrefix}.js.map`, './register/index.js.map'));
       skipIndex = true;
     }
-    buildTasks.push(getBuildTask(`${fnPrefix}.js`, plugins,polyfills[0]));
+    buildTasks.push(getBuildTask(`${fnPrefix}.js`, plugins, polyfills[0]));
   }
-  if(!skipIndex && (isDefault || env.npm || env.prod)){
-    buildTasks.push(getBuildTask("index.js", [],polyfills[1]));
+  if (!skipIndex && (isDefault || env.npm || env.prod)) {
+    buildTasks.push(getBuildTask("index.js", [cauldron], polyfills[1]));
   }
   if (env.prod) {
     buildTasks.push(getBuildTask(fnPrefix + ".min.js", [
+      cauldron,
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
@@ -75,7 +99,7 @@ module.exports = (env)=>{
       }),
       new webpack.optimize.OccurrenceOrderPlugin(),
       new webpack.optimize.AggressiveMergingPlugin()
-    ],polyfills[2]));
+    ], polyfills[2]));
   }
   return buildTasks;
 };
