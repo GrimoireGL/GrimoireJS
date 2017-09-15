626de1a7d64f1584429bbb866adfaba1352e7da2
moajo <mimirosiasd@gmail.com>
2017-09-15 17:17:24 +0900

    [add] string converter test

--- test/Converters/StringConverterTest.ts
+++ test/Converters/StringConverterTest.ts
@@ -3,4 +3,6 @@ import StringConverter from "../../src/Converters/StringConverter";
 
 test("StringConverter should convert collectly", t => {
   t.truthy(StringConverter("HELLO") === "HELLO");
+  t.truthy(StringConverter(null) === null);
+  t.truthy(StringConverter(123) === "123");
 });
