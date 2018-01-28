48fc511c5069dbc83c2ac2802014a824bf5757f5
moajo <mimirosiasd@gmail.com>
2018-01-28 17:22:04 +0900

    fix: remove deprecated converter

--- src/Converter/NumberArrayConverter.ts
+++
@@ -1,9 +0,0 @@
-export default function NumberArrayConverter(val: any): any {
-  if (val instanceof Array) {
-    return val;
-  }
-  if (typeof val === "string") {
-    const splitted = val.split(",");
-    return splitted.map(s => Number.parseFloat(s));
-  }
-}
